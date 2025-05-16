be6e8898d2943330defa563b0e57403901bba83b
Philipp <129884416+bonsaibauer@users.noreply.github.com>
2025-05-16 14:41:03 +0200

    Anmkerung hinzugefügt

--- 4_Semester/python_course/algo1.py
+++ 4_Semester/python_course/algo1.py
@@ -4,6 +4,8 @@ import soundfile as sf
 import pyaudio
 import matplotlib.pyplot as plt
 
+# Algo 1
+
 def run(value, value2, fig, fileName, saveValue=None):
     # Audiodatei lesen
     data, fs = sf.read(fileName)
