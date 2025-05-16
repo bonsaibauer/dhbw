d72e26dc03a409adccaa3896770ad70d70ed4963
Philipp <129884416+bonsaibauer@users.noreply.github.com>
2025-05-16 10:58:07 +0200

    Create beginner.py

---
+++ 4_Semester/python_course/beginner.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+def run(value,value2,fig,a=None,b=None):
+    fig.clf()
+    fig.tight_layout()
+    fig.suptitle("Test: "+str(value+value2))
+    fig.canvas.draw()
+
+if __name__ == '__main__':
+    run(10,0,plt.figure())
+    plt.show()
